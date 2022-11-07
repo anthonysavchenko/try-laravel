6dbd88680f31bafabf41d23c85644734ef9e430a
Anthony Savchenko <anthony.savchenko@gmail.com>
2022-11-07 14:19:20 +0600

    Breeze and React installed

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,13 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/js/app.jsx', 'public/js')
+    .react()
+    .postCss('resources/css/app.css', 'public/css', [require('tailwindcss'), require('autoprefixer')])
+    .alias({
+        '@': 'resources/js',
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+}
